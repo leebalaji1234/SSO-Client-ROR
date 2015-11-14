29ce367133a854be79f702b0bea30b819e283bc6
Balaji Arumugam <balaji.arumugam@bigtappanalytics.com>
2015-11-14 12:07:19 +0530

    CORS Enabled - Missing file

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'dbconnection' => 'settings#db'
 
   # devise_for :users
-  devise_for :users, :controller => {sessions: 'sessions'}
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
